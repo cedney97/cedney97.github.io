770a82b8640a1e54e300c26ca3ff15d3f2a3424e
cedney97 <c.edney@wustl.edu>
2023-05-28 00:24:44 -0500

    Home page mostly working

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,38 @@
+import { useState, useEffect } from 'react'
+import { TypeAnimation } from 'react-type-animation'
+
 import styles from '../styles/home.module.css'
 
 const HomePage = () => {
+
+    const [textColor, setTextColor] = useState('white')
+
     return (
-        <div className={styles.home_container}>Home</div>
+        <div className={styles.home_container}>
+            <div style={{
+                textAlign: 'center',
+                fontSize: '5em',
+                color: textColor,
+                textShadow: '2px 2px 2px var(--black-sub)'
+            }}>
+                <TypeAnimation
+                    sequence={[
+                        'Hello There!',
+                        2000,
+                        () => setTextColor('var(--reddish)'),
+                        'I\'m Cade Edney!',
+                        5000,
+                        () => setTextColor('white'),
+                        'Scroll down for\nmore info!',
+                        5000
+                    ]}
+                    repeat={Infinity}
+                    omitDeletionAnimation={true}
+                    className={styles.text_wrapper}
+                    speed={20}
+                />
+            </div>
+        </div>
     )
 }
 
