9e251061b40ad995c21a316ad049461ff37fcd6e
cedney97 <c.edney@wustl.edu>
2023-05-31 12:17:53 -0500

    Working on Sidenav linking to pages

--- src/components/details/Sidenav.js
+++ src/components/details/Sidenav.js
@@ -1,5 +1,11 @@
 import styles from '../../styles/navbar.module.css'
 
+import { Link } from 'react-router-dom'
+import AboutPage from '../pages/AboutPage'
+import ContactPage from '../pages/ContactPage'
+import HomePage from '../pages/HomePage'
+import ProjectsPage from '../pages/ProjectsPage'
+
 const Sidenav = ({
     navOpen,
     setNavOpen
@@ -16,11 +22,11 @@ const Sidenav = ({
     return (
         <div>
             <div className={`${styles.sidenav} ${navOpen ? styles.open : styles.closed}`}>
-                <a href="#!" className={styles.closebtn} onClick={closeNav}>&times;</a>
-                <a href="/" className={styles.menu_item}>Home</a>
-                <a href="/" className={styles.menu_item}>About</a>
-                <a href="/" className={styles.menu_item}>Projects</a>
-                <a href="/" className={styles.menu_item}>Contact</a>
+                <Link href="#!" className={styles.closebtn} onClick={closeNav}>&times;</Link>
+                <Link href="/" component={HomePage} className={styles.menu_item}>Home</Link>
+                <Link href="/" component={AboutPage} className={styles.menu_item}>About</Link>
+                <Link href="/" component={ProjectsPage} className={styles.menu_item}>Projects</Link>
+                <Link href="/" component={ContactPage} className={styles.menu_item}>Contact</Link>
             </div>
             <button className={styles.openbtn} onClick={navOpen ? closeNav : openNav} />
         </div >
